4a6fc6b7d266218f05f49362b210f6611fe82276
Ronny Endara <rojaence@gmail.com>
2024-10-13 14:23:44 -0500

    UPDATE dashboard, ADD newUserDialog

---
+++ frontend/src/app/components/users/new-user/new-user.component.ts
@@ -0,0 +1,85 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
+
+@Component({
+  selector: 'app-new-user',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, CommonModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, ReactiveFormsModule],
+  templateUrl: './new-user.component.html',
+  styleUrl: './new-user.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideNativeDateAdapter()],
+
+})
+export class NewUserComponent implements OnInit {
+  constructor(private newUserDialogRef: MatDialogRef<NewUserComponent>, private fb: FormBuilder){}
+
+  profileForm!: FormGroup;
+
+  closeDialog() {
+    this.newUserDialogRef.close();
+  }
+
+  ngOnInit() {
+    this.createForm();
+  }
+  createForm() {
+    this.profileForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20), Validators.pattern(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]+$/)]],
+      firstName: ['', [Validators.required]],
+      middleName: ['', [Validators.required]],
+      firstLastname: ['', [Validators.required]],
+      secondLastname: ['', [Validators.required]],
+      idCard: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern(/^(?!.*(\d)\1{3})\d{10}$/)]],
+      birthDate: [new Date(), [Validators.required]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*\W)(?!.*\s).+$/), Validators.maxLength(100), Validators.minLength(8)]],
+    })
+  }
+
+  getErrorMessage(controlName: string): string | null {
+    let control = this.profileForm.get(controlName);
+    if (!control) return '';
+    if (control.errors) {
+      if (control.hasError('required')) {
+        return 'Este campo es obligatorio.';
+      }
+      if (control.hasError('minlength')) {
+        const requiredLength = control.errors['minlength'].requiredLength;
+        return `Debe tener al menos ${requiredLength} caracteres.`;
+      }
+      if (control.hasError('maxlength')) {
+        const requiredLength = control.errors['maxlength'].requiredLength;
+        return `Debe tener máximo ${requiredLength} caracteres.`;
+      }
+      if (control.hasError('pattern')) {
+        if (controlName == 'username') {
+          return 'El nombre de usuario debe contener al menos una letra mayúscula y un número, y no puede contener signos.';
+        }
+        if (controlName == 'idCard') {
+          return 'La identificación debe tener solo números y no puede tener 4 veces seguidas el mismo.';
+        }
+      }
+      if (control.hasError('pattern')) {
+        if (controlName == 'password') {
+          return 'La contraseña debe contener al menos una letra mayúscula, un signo y no debe contener espacios.';
+        }
+      }
+    }
+    return null;
+  }
+
+  get f() {
+    return this.profileForm.controls;
+  }
+
+  onSubmit() {
+
+  }
+}
--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -3,16 +3,24 @@ import { UserService } from '../../services/user.service';
 import { IUserItemDTO } from '../../models/user';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { NewUserComponent } from '../../components/users/new-user/new-user.component';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatTableModule, CommonModule],
+  imports: [MatTableModule, CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
-  constructor(private userService: UserService) {}
+
+  constructor(private userService: UserService,
+    public dialog: MatDialog
+  ) {}
 
   users: IUserItemDTO[] = [];
   displayedColumns: string[] = ['username', 'fullname', 'status', 'sessionActive', 'lastSession', 'role'];
@@ -20,7 +28,6 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.userService.getUsers().subscribe({
       next: (users) => {
-        console.log(users)
         this.users = users;
       },
       error: (err) => {
@@ -32,5 +39,12 @@ export class DashboardComponent implements OnInit {
   getUserName(idUser: number) {
     let user = this.users.find(u => u.idUser === idUser);
     return `${user?.firstName} ${user?.firstLastname} ${user?.secondLastname}`;
+  }
+
+  openNewUserDialog() {
+    const dialogRef = this.dialog.open(NewUserComponent, {
+      maxWidth: '900px',
+      disableClose: true,
+    });
   }
 }
